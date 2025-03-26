9ff4463110749181308759b93990e0ba7b797b5f
tahirdotz <tahirzaman075@gmail.com>
2025-03-26 21:11:17 +0600

    Added return hashcpde

--- src/Coordinates.java
+++ src/Coordinates.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Coordinates {
     private final double latitude;
     private final double longitude;
@@ -13,5 +15,18 @@ public class Coordinates {
 
     public double getLongitude() {
         return longitude;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinates that = (Coordinates) o;
+        return Double.compare(latitude, that.latitude) == 0 && Double.compare(longitude, that.longitude) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(latitude, longitude);
     }
 }
